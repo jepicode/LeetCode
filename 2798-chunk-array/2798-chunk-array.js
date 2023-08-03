c551208d48156b0ae18c5d888099b03f98d7ed12
Jepi usuluddin <jepi.usuluddin@gmail.com>
2023-08-04 03:36:21 +0700

    Time: 54 ms (90.47%), Space: 45.2 MB (9.3%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -5,7 +5,6 @@
  */
 var chunk = function(arr, size) {
     const values = [];
-    let tempCounter = 0;
     for (let i = 0; i < Math.ceil(arr.length / size); i++) {
         values.push([...arr].splice(i * size, size));
     }
