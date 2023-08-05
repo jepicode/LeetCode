97e3b822fffa62a31af5781b4032c1e8e3c2a41a
Jepi usuluddin <jepi.usuluddin@gmail.com>
2023-08-05 22:23:49 +0700

    Time: 56 ms (95.72%), Space: 44.1 MB (45.65%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -6,11 +6,16 @@ var reverse = function(x) {
     const INT_MIN = -Math.pow(2, 31);
     const INT_MAX = Math.pow(2, 31) - 1;
 
-    let str = Math.abs(x).toString().split('').reverse().join('');
-    let reversed = Number(str) * Math.sign(x);
+    let reversed = 0;
 
-    if (reversed < INT_MIN || reversed > INT_MAX) {
-        return 0;
+    while (x !== 0) {
+        const digit = x % 10;
+        x = (x / 10) | 0;  // Membuang digit terakhir
+        // Memeriksa batasan integer sebelum mengalikan dengan 10
+        if (reversed > INT_MAX / 10 || (reversed === INT_MAX / 10 && digit > 7)) return 0;
+        if (reversed < INT_MIN / 10 || (reversed === INT_MIN / 10 && digit < -8)) return 0;
+        reversed = reversed * 10 + digit;
     }
+
     return reversed;
 };
