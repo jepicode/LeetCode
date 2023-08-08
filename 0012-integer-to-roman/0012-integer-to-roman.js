13acb00f9646d43d2ad4f22e2988af650491dd3e
Jepi usuluddin <jepi.usuluddin@gmail.com>
2023-08-09 00:07:49 +0700

    Time: 237 ms (5.36%), Space: 53 MB (5.17%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -5,62 +5,62 @@
 var intToRoman = function(nums) {
     let str = '';
     const transformNumber = (num) => {
-        console.log(num);
+        console.log('num', num);
         if (num <= 0) return str;
-        if (num / 1000 >= 1) {
-            str += 'M';
-            return transformNumber(num - 1000);
+        if (num >= 1000) {
+            while (num >= 1000) {
+                str += 'M';
+                num -= 1000;
+            }
+            return transformNumber(num);
         }
-        if (num / 100 >= 1) {
-            const hundred = Math.floor(num / 100);
-            console.log(hundred);
-            if (hundred === 9) {
-                str += 'CM';
-                return transformNumber(num - 900);
+        if (num >= 100) {
+            while (num >= 100) {
+                if (num >= 900) {
+                    str += 'CM'; num -= 900;
+                }
+                if (num >= 500) {
+                    str += 'D'; num -= 500;
+                }
+                if (num >= 400) {
+                    str += 'CD'; num -= 400;
+                }
+                if (num >= 100) {
+                    str += 'C'; num -= 100;
+                }
             }
-            if (hundred >= 5) {
-                str += 'D';
-                return transformNumber(num - 500);
-            }
-            if (hundred === 4) {
-                str += 'CD';
-                return transformNumber(num - 400);
-            }
-            str += 'C';
-            return transformNumber(num - 100);
+            return transformNumber(num);
         }
-        if (num / 10 >= 1) {
-            const dozens = Math.floor(num / 10);
-            console.log('dozens', dozens);
-            if (dozens === 9) {
-                str += 'XC';
-                return transformNumber(num - 90);
+        if (num >= 10) {
+            while (num >= 10) {
+                if (num >= 90) {
+                    str += 'XC'; num -= 90;
+                }
+                if (num >= 50) {
+                    str += 'L'; num -= 50;
+                }
+                if (num >= 40) {
+                    str += 'XL'; num -= 40;
+                }
+                if (num >= 10) {
+                    str += 'X'; num -= 10;
+                }
             }
-            if (dozens >= 5) {
-                str += 'L';
-                return transformNumber(num - 50);
-            }
-            if (dozens === 4) {
-                str += 'XL';
-                return transformNumber(num - 40);
-            }
-            str += 'X';
-            return transformNumber(num - 10);
+            return transformNumber(num);
         }
-        if (num === 9) {
-            str += 'IX';
-            return transformNumber(num - 9);
+        if (num >= 9) {
+            str += 'IX'; num -= 9;
         }
         if (num >= 5) {
-            str += 'V';
-            return transformNumber(num - 5);
+            str += 'V'; num -= 5;
         }
-        if (num === 4) {
-            str += 'IV';
-            return transformNumber(num - 4);
+        if (num >= 4) {
+            str += 'IV'; num -= 4;
         }
-        str += 'I';
-        return transformNumber(num - 1);
+        if (num >= 1) {
+            str += 'I'; num -= 1;
+        }
+        return transformNumber(num);
     };
 
     return transformNumber(nums);
